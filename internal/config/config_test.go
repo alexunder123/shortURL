3709deffc66b05984330661690bca4e877143200
alex_under <baranov.al.a@yandex.ru>
2022-12-28 23:52:59 +0700

    Инкремент №10.1

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -16,8 +16,9 @@ func TestGetEnv(t *testing.T) {
 			want: &Param{
 				Server:   "127.0.0.1:8080",
 				URL:      "http://127.0.0.1:8080",
-				Storage:  "jsonDB.json",
-				SaveFile: 1,
+				Storage:  "json_storage.json",
+				SQL: "PostgreSQL:localhost",
+				SaveFile: 2,
 			},
 		},
 	}
@@ -25,7 +26,8 @@ func TestGetEnv(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
 			os.Setenv("BASE_URL", "http://127.0.0.1:8080")
-			os.Setenv("FILE_STORAGE_PATH", "jsonDB.json")
+			os.Setenv("FILE_STORAGE_PATH", "json_storage.json")
+			os.Setenv("DATABASE_DSN", "PostgreSQL:localhost")
 			if got := NewConfig(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetEnv() = %v, want %v", got, tt.want)
 			}
